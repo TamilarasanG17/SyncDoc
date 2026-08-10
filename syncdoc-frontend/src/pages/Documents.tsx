b42e420c4ed3d711b8bb775d38088a8beeccdc25
TamilarasanG17 <tamilarasang17@gmail.com>
2026-08-10 16:10:44 +0530

    feat : Create the pages like documents,home,editor

---
+++ syncdoc-frontend/src/pages/Documents.tsx
@@ -0,0 +1,10 @@
+function Documents() {
+  return (
+    <div>
+      <h2>Documents</h2>
+      <p>Your documents will appear here.</p>
+    </div>
+  );
+}
+
+export default Documents;
---
+++ syncdoc-frontend/src/pages/Editor.tsx
@@ -0,0 +1,10 @@
+function Editor() {
+  return (
+    <div>
+      <h2>SyncDoc Editor</h2>
+      <p>Editor area</p>
+    </div>
+  );
+}
+
+export default Editor;
