32f26cfaca32562e2e2d0d2fa58a7bde59d4dc28
Suhani510-art <mahalesukrati@gmail.com>
2026-08-10 22:24:35 +0530

    feat: initialize SyncDoc backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log("Server is running...")
+const express = require("express");
+
+const app = express();
+
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+    res.send("SyncDoc Backend Running");
+});
+
+app.listen(PORT, () => {
+    console.log(`SyncDoc Backend running on port ${PORT}`);
+});
