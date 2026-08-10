01f844bf81c384e5b344101ea211067bfee91a2e
TamilarasanG17 <tamilarasang17@gmail.com>
2026-08-10 16:09:36 +0530

    feat : Create the components like header sidebar etc.

---
+++ syncdoc-frontend/src/components/layout/Header.tsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1>SyncDoc</h1>
+
+      <div className="header-actions">
+        <span>My Workspace</span>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
