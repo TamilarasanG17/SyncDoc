2a21fed57096eeaec2e19dc198e37b18c94895b9
Suhani510-art <mahalesukrati@gmail.com>
2026-08-12 00:10:58 +0530

    feat: setup MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,22 @@
+require("dotenv").config();
+
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
     res.send("SyncDoc Backend Running");
 });
 
-app.listen(PORT, () => {
-    console.log(`SyncDoc Backend running on port ${PORT}`);
-});
+const startServer = async () => {
+    await connectDB();
+
+    app.listen(PORT, () => {
+        console.log(`SyncDoc Backend running on port ${PORT}`);
+    });
+};
+
+startServer();
